46f4c2e7f350f763e830089b689e296d82cb28fb
tania31587 <inf31587@adobe.com>
2026-09-04 22:50:54 +0530

    fixes

--- blocks/related-products/related-products.js
+++ blocks/related-products/related-products.js
@@ -5,8 +5,16 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const card = document.createElement('article');
     card.className = 'product-card';
+    const picture = row.querySelector('picture');
     const content = document.createElement('div');
     content.className = 'product-card-content';
+
+    if (picture) {
+      const media = document.createElement('div');
+      media.className = 'product-card-media';
+      media.append(picture);
+      card.append(media);
+    }
 
     [...row.children].forEach((cell) => {
       while (cell.firstElementChild) content.append(cell.firstElementChild);
